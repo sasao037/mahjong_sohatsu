1073239b7123efbb3cba5be73bfeebb53ed54b67
sasao reo <sasao@lifematics.co.jp>
2023-02-17 16:22:57 +0900

    reset

--- sohatsu/docs/js/about.e69c476f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunksohatsu"]=self["webpackChunksohatsu"]||[]).push([[443],{403:function(s,t,u){u.r(t),u.d(t,{default:function(){return i}});var n=function(){var s=this;s._self._c;return s._m(0)},a=[function(){var s=this,t=s._self._c;return t("div",{staticClass:"about"},[t("h1",[s._v("This is an about page")])])}],e=u(1),l={},r=(0,e.Z)(l,n,a,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about.e69c476f.js.map
