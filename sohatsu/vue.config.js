ca093d53cd0d00f2098063b86a0f9fd7a9b5f86c
sasao reo <sasao@lifematics.co.jp>
2023-02-17 10:26:50 +0900

    feat: change port

--- sohatsu/vue.config.js
+++ sohatsu/vue.config.js
@@ -1,6 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: [
+//     'vuetify'
+//   ]
+// })
+
+
+module.exports = {
+    devServer: {
+        port: 8030,
+    },
+};
